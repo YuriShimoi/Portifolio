5e2fab218b6c1eeba80d1b5acc6dbc5eb2b806f8
Yuri Shimoi <shimoi.yuri@gmail.com>
2022-12-20 00:43:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+async function getPinnedRepos(f) {
+    await fetch("https://gh-pinned-repos.egoist.dev/?username=YuriShimoi")
+    .then((res) => res.json()).then((data) => {
+        return f(data);
+    });
+}
+
+function clickItem(e) {
+    return e.getElementsByTagName('a')[0].click();
+}
+
+function buildProjectItem(title, description, link, stars=0) {
+    let pitem = buildHTML("DIV", null, "project-item", (e) => {clickItem(e)});
+
+    let primg = buildHTML("IMG");
+    primg.src = "images/githubpage.png";
+    primg.style = "--filter: grayscale(1) brightness(0.6) contrast(1.5)";
+
+    let telem = buildHTML("H1");
+    let tlink = buildHTML("A");
+    tlink.href      = link;
+    tlink.target    = "_target";
+    tlink.innerHTML = title;
+    if(stars > 0) tlink.innerHTML += `<span class="project-starts">${stars}</span>`;
+
+    let resum = buildHTML("DIV", null, "project-resume");
+    resum.innerHTML = description?? " ";
+    
+    pitem.appendChild(primg);
+
+    telem.appendChild(tlink);
+    pitem.appendChild(telem);
+
+    pitem.appendChild(resum);
+
+    return pitem;
+}
+
+function buildHTML(element, eid=null, classes=null, onclick=null) {
+    let new_element = document.createElement(element);
+    if(eid)     new_element.id = eid;
+    if(classes) new_element.classList.add(classes);
+    if(onclick) new_element.onclick = () => {onclick(new_element)};
+    return new_element;
+}
+
+getPinnedRepos((repos) => {
+    let plist = document.getElementById("pinned-projects");
+    plist.innerHTML = "";
+    for(let r in repos) {
+        let rep = repos[r];
+        plist.appendChild(buildProjectItem(rep.repo, rep.description, rep.link, rep.stars));
+    }
+});
